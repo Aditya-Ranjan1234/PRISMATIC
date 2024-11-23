627b3431b2099cc07016f4323053268d5b18f961
Aditya Ranjan <136574821+Aditya-Ranjan1234@users.noreply.github.com>
2024-11-23 16:04:05 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,6 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Main from "../mainPage/Main";
 import MobileWarning from "./MobileWarning";
-import "../../public/styles.css";
 
 function App() {
   const isMobile = /Mobi|Android/i.test(navigator.userAgent);
