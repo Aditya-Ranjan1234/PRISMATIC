28f027b811fb66d6d9db4437fdf30d968f26c82e
Aditya Ranjan <136574821+Aditya-Ranjan1234@users.noreply.github.com>
2024-11-25 19:28:18 +0530

    Update Uploader.jsx

--- src/mainPage/Uploader.jsx
+++ src/mainPage/Uploader.jsx
@@ -9,7 +9,7 @@ function Uploader() {
   const [userPrompt, setUserPrompt] = useState("");
   const [imageDescription, setImageDescription] = useState("");
   const [loading, setLoading] = useState(false);
-  const BASE_API_URL = "https://0dfe-103-186-121-122.ngrok-free.app"; 
+  const BASE_API_URL = "https://0981-2401-4900-6304-28ba-2864-5a8e-7770-eb07.ngrok-free.app"; 
 
   const onDrop = useCallback((acceptedFiles) => {
     const newFiles = acceptedFiles.map((file) =>
